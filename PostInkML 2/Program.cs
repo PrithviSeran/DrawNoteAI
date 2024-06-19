790531c124c686ae8a3395333dcf885730f2ad62
prithviseran <prithisha33@gmail.com>
2024-06-18 21:07:02 -0400

    why

---
+++ PostInkML 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+/*
+using (var client = new HttpClient())
+{
+    string token = "EwCYA8l6BAAUbDba3x2OMJElkF7gJ4z/VbCPEz0AATo88AnQfludYcHqteQsHZO82XMxgaayspCv/BnNSCa8Pj7L6/EozMPdMVBg9ycL1s4U8VvScLSJ1ehJrbuY/EmqTaLIwu/uhyuDfEfrNOqqFKfMIHnYicGUDaqv92KtGsx29UwMrdubAzgIJgGHfgavg3C7KRU8f/m86LKb8ZdHdcd4LeaYKSBzJ1ztKwqCNRByUkLsSRRXWDtP75+a08A37z55KxpFS3JCvt2jT5m4C01zEpd2HqcDCgOtNXdupDKfEXpUQe/V7QGifOAE6ZTbWkxyeadwprckkOUv+0TXBhmMdjGkvvQdxYekzUnCxWzaXRv9r0Rxg/R7BTHvWJ4DZgAACGYTRB5BvuNdaAIuYo5YXE+XPJt7VrdcIDmTLcwJPJYCHiQhk4NExls3MqGFOt5OB8aqpz6cnPX39I52NMs4HsN2j0rhfMD2cBe/QvF1spKOv1cwRYXcwxW226jRQYqWG2GNZ5Srr/5j/DoCdqlitCowDn5zPrdBnD9MzOtJJnxXEybBKJk1FHuDZZcWYQYn/cQHbSFFgVuvRKFgrY/hR33RskyEdYTJCbO5jVEklAlgdcfdr30Twi8u1pkCgQy/O6ZGSs2PKsnAVxtXBOQS4ouwzptbGDl9dh8CdG92o69TiF/6adhPQF3cFiQ1SaVrF0MCicwxAJOanJ4AQJkRW/C8cpj2PnobAEBxjCxYgsLkj1Wc8D+A6Zh5xjXFQINuUWIQhr774oFyr6sfqYsC0LGqpmMiwEd5f2oEPcmCXHSTOGroLUdBgRjexoGmykLkZgnnCNOcpn9PvL5RHIdwWdz3WGbMA+4rW6JQ3XGweK8JJ2aEA22D3roPfcRcX9/2Zcj09s8V+lvjeywXZ4nH0OmJRvlvUz5Y3sdSJXaRbdv4UFSEEyg9fQbBVyWlmTQ/HEe7GfMeD0MJ5IKXpbhPi1zlMSibxASNwtYLvhmnK9VeqIFhIwN/wy8DRVmWk2OLapWC/FdzK97sra8HVpmBBdeLM+7oOwCmcDpr2qedbqtVQZ2Y/cEjA8b7i12EnZBTVLPzlgrAFCZ5hnCJIkWHjNp++VOFD6bTB6uLQWrZy2IznNUBAI+/d9Dt6Czavxa5aMo8Di/N3wJaSgMqlrgcbSTHip4sT+l0hwI5me8V8+BhCWo3BBE55bV1gnlt/HQKJNcWpgI=";
+
+    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+    using (var content = new MultipartFormDataContent("MyPartBoundary198374"))
+    {
+        var stringContent = new StringContent("<h1>Hello</h1>", System.Text.Encoding.UTF8, "text/html");
+        var inkml = new StringContent(File.ReadAllText(@"inkML.txt"),
+            System.Text.Encoding.UTF8,
+            "application/inkml+xml");
+
+        
+        content.Add(stringContent, "presentation");
+        //-onenote-inkml");
+        content.Add(inkml, "presentation-onenote-inkml");
+
+
+        using (
+           var message =
+               await client.PostAsync("https://graph.microsoft.com/v1.0/me/onenote/sections/0-A213A7A8D7193228!82019/pages", content))
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+*/
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services => {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+    })
+    .Build();
+
+host.Run();
+
---
+++ PostInkML 2/RecieveInkML.cs
@@ -0,0 +1,123 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+
+namespace Prince.InkML
+{
+    public class RecieveInkML
+    {
+        private readonly ILogger<RecieveInkML> _logger;
+
+        public RecieveInkML(ILogger<RecieveInkML> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function(nameof(RecieveInkML))]
+        public async Task Run([BlobTrigger("rundnnmodel/inkML.xml", Connection = "rundnnmodel_STORAGE")] Stream stream, string name)
+        {
+            using var blobStreamReader = new StreamReader(stream);
+            var content = await blobStreamReader.ReadToEndAsync();
+
+            var client = new HttpClient();
+            _logger.LogInformation($"C# Blob trigger function Processed blob\n Name: {name} \n");
+
+            string connectionString = @"DefaultEndpointsProtocol=https;AccountName=princenotes2;AccountKey=WyXAD4Ie8/JPhIe8igMqUwoM9/m+pmgnPZA36ZSicdt8xVjlJoIc4Zaq3Ti+jaWId7po/SxdFloi+AStnYkH4g==;EndpointSuffix=core.windows.net";
+            //$"DefaultEndpointsProtocol=https;AccountName={storageAccountName};AccountKey={storageAccountKey};EndpointSuffix=core.windows.net";
+
+            // Create a BlobServiceClient
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            // Get a reference to the container
+            string containerName = "rundnnmodel";
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            // Get a reference to the blob
+            string blobName = "authtoken.txt";
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            string UserImage = "user_image.jpg";
+            BlobClient blobClientUserImage = containerClient.GetBlobClient(UserImage);
+
+            string inkML_data = "inkML.xml";
+            BlobClient blobClientInkML = containerClient.GetBlobClient(inkML_data);
+
+            string pageName = "pageName.txt";
+            BlobClient pageNameBlob = containerClient.GetBlobClient(pageName);
+
+            string sectionId = "sectionId.txt";
+            BlobClient sectionIdBlob = containerClient.GetBlobClient(sectionId);
+
+
+            // Download the blob's contents and print it
+            BlobDownloadInfo download = await blobClient.DownloadAsync();
+
+            string contents = "";
+
+            using (StreamReader reader = new StreamReader(download.Content, true))
+            {
+                contents = await reader.ReadToEndAsync();
+                Console.WriteLine(contents);
+            }
+
+            BlobDownloadInfo downloadPage = await pageNameBlob.DownloadAsync();
+
+            string contentsPageName = "";
+
+            using (StreamReader reader = new StreamReader(downloadPage.Content, true))
+            {
+                contentsPageName = await reader.ReadToEndAsync();
+                Console.WriteLine(contentsPageName);
+            }
+
+            BlobDownloadInfo downloadSection = await sectionIdBlob.DownloadAsync();
+
+            string contentsSection = "";
+
+            using (StreamReader reader = new StreamReader(downloadSection.Content, true))
+            {
+                contentsSection = await reader.ReadToEndAsync();
+                Console.WriteLine(contentsSection);
+            }
+
+            
+            string token = contents;
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            var content2 = new MultipartFormDataContent("MyPartBoundary198374");
+            var stringContent = new StringContent($"<h1>{contentsPageName}</h1>", System.Text.Encoding.UTF8, "text/html");
+            var inkml = new StringContent(content,
+                System.Text.Encoding.UTF8,
+                "application/inkml+xml");
+
+            
+            content2.Add(stringContent, "presentation");
+            //-onenote-inkml");
+            content2.Add(inkml, "presentation-onenote-inkml");
+
+
+            using (
+            var message =
+                await client.PostAsync($"https://graph.microsoft.com/v1.0/me/onenote/sections/{contentsSection}/pages", content2))
+            {
+                Console.WriteLine(message);
+            }
+
+            //static async Task DeleteBlobAsync(BlobClient blobClientUserImage)
+            //{
+            await blobClientUserImage.DeleteAsync();
+            await blobClientInkML.DeleteAsync();
+            //}
+            
+        }
+
+    
+    }
+    
+}
+
+
